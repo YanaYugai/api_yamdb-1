e3dbdeff1f94a064d742f7ba3f4686220e52cd18
Мария Тумилович <khamaran@yahoo.com>
2023-04-12 15:50:28 +0300

    Обновила загрузчик csv

--- api_yamdb/reviews/management/commands/load_csv.py
+++ api_yamdb/reviews/management/commands/load_csv.py
@@ -1,40 +1,33 @@
-import csv
-from django.utils import timezone
-from django.core.management import BaseCommand
+from csv import DictReader
+from django.conf import settings
+from django.core.management.base import BaseCommand
 
-from reviews.models import Category, Genre, Title
+from reviews.models import (Category, Comment, Genre, GenreToTitle, Review,
+                            Title, User,)
+
+MODEL_CSV = {
+    User: 'users.csv',
+    Category: 'category.csv',
+    Genre: 'genre.csv',
+    Title: 'titles.csv',
+    Review: 'review.csv',
+    Comment: 'comments.csv',
+    GenreTitle: 'genre_title.csv'
+}
 
 
 class Command(BaseCommand):
-    """ Загрузчик csv файлов."""
-    help = "Loads csv files"
+    """Загрузчик данных csv. """
 
-    def add_arguments(self, parser):
-        parser.add_argument("file_path", type=str)
+    help = 'Import .csv files'
 
     def handle(self, *args, **options):
-        start_time = timezone.now()
-        file_path = options["file_path"]
-        with open(file_path, "r") as csv_file:
-            data = list(csv.reader(csv_file, delimiter=","))
-            for row in data[1:]:
-                compositions_category = Category.objects.get_or_create(
-                    pk=row[0],
-                    name=row[1],
-                    slug=row[2],
-                )
-                compositions_genre = Genre.objects.get_or_create(
-                    pk=row[0],
-                    name=row[1],
-                    slug=row[2],
-                )
-                compositions = Title.objects.get_or_create(pk=row[0],
-                                                           name=row[1],
-                                                           year=row[2],
-                                                           category=compositions_category[0])
-        end_time = timezone.now()
-        self.stdout.write(
-            self.style.SUCCESS(
-                f"Loading CSV took: {(end_time-start_time).total_seconds()} seconds."
-            )
-        )
+        for model, csv in MODEL_CSV.items():
+            with open(
+                    f'{settings.BASE_DIR}/static/data/{csv}',
+                    'r', encoding='utf-8'
+            ) as csv_file:
+                reader = DictReader(csv_file)
+                model.objects.bulk_create(model(**data) for data in reader)
+
+        self.stdout.write(self.style.SUCCESS('Данные из .csv файлов загружены успешно!'))
